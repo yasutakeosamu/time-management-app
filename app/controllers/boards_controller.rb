e3fcd72bddc7b56139c21d5426da61cec4ca8379
yasutakeosamu <036yasutake@gmail.com>
2020-10-01 13:50:42 +0900

    createアクションを定義

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -1,4 +1,10 @@
 class BoardsController < ApplicationController
   def index
   end
+
+  def create
+    @board = Board.new(name:params[:name],user_id:current_user.id)
+    redirect_to root_path if @board.save
+  end
+
 end
