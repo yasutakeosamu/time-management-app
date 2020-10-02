dc61ff22a053780cc35198c475fa950b5e951d15
yasutakeosamu <036yasutake@gmail.com>
2020-10-02 17:32:29 +0900

    boardとtaskのindexのビューを編集

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,4 +1,6 @@
 class TasksController < ApplicationController
   def index
+    @boards = Board.all
+    @board = Board.find(params[:board_id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,7 @@ Rails.application.routes.draw do
   get 'tasks/index'
   devise_for :users
   root 'boards#index'
-  resources :boards, only: [:create] do
-    resources :tasks, only: [:index, :new, :create]
-  end
+  resources :boards, only: [:create] 
+  resources :tasks, only: [:index, :new, :create]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
