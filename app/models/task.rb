f619867c32e7ff322bb379def8445aaf4ab292cc
yasutakeosamu <036yasutake@gmail.com>
2020-10-03 23:00:39 +0900

    モデルテスト

--- app/models/task.rb
+++ app/models/task.rb
@@ -3,4 +3,11 @@ class Task < ApplicationRecord
   belongs_to :board
 
   validates :title, :inportance, :deadline, :hour, presence:true
+  validate :before_today
+
+  private
+
+  def before_today
+    errors.add(:deadline, "can't be the post") if deadline != nil && deadline < Date.today 
+  end
 end
--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -1,5 +1,41 @@
 require 'rails_helper'
 
 RSpec.describe Task, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '#create' do
+    before do
+      @task = FactoryBot.build(:task)
+    end
+    it '正しく入力されれば登録できる' do
+      expect(@task).to be_valid
+    end
+    it 'titleが空では登録できない' do
+      @task.title = ''
+      @task.valid?
+      expect(@task.errors.full_messages).to include("Title can't be blank")
+    end
+    it 'textは空でも登録できる' do
+      @task.text = ''
+      expect(@task).to be_valid
+    end
+    it 'inportanceが空では登録できない' do
+      @task.inportance = ''
+      @task.valid?
+      expect(@task.errors.full_messages).to include("Inportance can't be blank")
+    end
+    it 'deadlineが空では登録できない' do
+      @task.deadline = ''
+      @task.valid?
+      expect(@task.errors.full_messages).to include("Deadline can't be blank")
+    end
+    it 'deadlineが過去の日付では登録できない' do
+      @task.deadline = Faker::Date.backward
+      @task.valid?
+      expect(@task.errors.full_messages).to include("Deadline can't be the post")
+    end
+    it 'hourが空では登録できない' do
+      @task.hour = ''
+      @task.valid?
+      expect(@task.errors.full_messages).to include("Hour can't be blank")
+    end
+  end
 end
